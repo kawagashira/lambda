86651c8532c5965f88e11670e919871329fa1b73
nobuyuki_kawagashira <79078-nobuyuki_kawagashira@users.noreply.gitlab.aicrowd.com>
2024-11-10 19:56:01 +0900

    addition

---
+++ addition.hs
@@ -0,0 +1,13 @@
+zero s z = z
+one s z = s(z)
+two s z = s(s(z))
+three s z = s(s(s(z)))
+
+succ' n a b = a (n a b)
+add1 n = n + 1
+
+main = do
+    print $ two succ' three add1 0
+    --print $ succ' zero add1 0
+    --print $ succ' one add1 0
+    --print $ succ' two add1 0
