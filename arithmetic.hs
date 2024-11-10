4685a6aa00a06faddc0f9c666c464ae4c10ef421
nobuyuki_kawagashira <79078-nobuyuki_kawagashira@users.noreply.gitlab.aicrowd.com>
2024-11-10 19:34:39 +0900

    successor = Lnab.a(nab)

--- arithmetic.hs
+++ arithmetic.hs
@@ -2,6 +2,14 @@ zero s z = z
 one s z = s(z)
 two s z = s(s(z))
 three s z = s(s(s(z)))
+
+succ' n a b = a (n a b)
 add1 n = n + 1
 
-main = do print $ three add1 0
+--main = do print $ three add1 0
+--main = do print $ (succ' three) add1 0
+--main = do print $ one add1 0
+main = do
+    print $ succ' zero add1 0
+    print $ succ' one add1 0
+    print $ succ' two add1 0
