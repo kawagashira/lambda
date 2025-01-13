76fe1347612829e89105a1d06ca47590cc07ec78
kawagashira <kawagashira@gmail.com>
2025-01-13 23:22:10 +0900

    Ackermann funciton & Fibonacci series

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ https://qiita.com/Trubetzkoy/items/169de0f152bca5f6c45c
 b_combinator        Bコンビネータ
 c_combinator        Cコンビネータ
 https://qiita.com/Trubetzkoy/items/7f2e6f9de5e07f50decf
+
+Set.hs              Set type
+SetOfSet.hs         hierarchical Set
+
+ackermann.hs        Ackermann's function
+fibonacci.hs        Fibonacci series
--- SetOfSets.hs
+++ SetOfSets.hs
@@ -4,11 +4,18 @@ type SetOfSets a = Set.Set (Set.Set a)
 singletonSetOfSets :: (Ord a) => Set.Set a -> SetOfSets a
 singletonSetOfSets s = Set.singleton s
 
-fromListSetOfSets :: (Ord a ) => [Set.Set a] -> SetOfSets a
+fromListSetOfSets :: (Ord a) => [Set.Set a] -> SetOfSets a
 fromListSetOfSets xs = Set.fromList xs
+
+insertSetOfSets :: (Ord a) => a -> [Set.Set a] -> [SetOfSets a]
+insertSetOfSets x xs = Set.insert x xs
 
 main = do
     let set1 = Set.fromList [1, 2]
     let set2 = Set.fromList [3, 4]
-    let s1 = singletonSetOfSets set1
-    print $ s1
+    --let set3 = singletonSetOfSets set2
+    print $ set1
+    let set4 = Set.fromList [set1, set2]
+    print $ set4
+    let set5 = Set.insert 5 set4
+    print $ set5
